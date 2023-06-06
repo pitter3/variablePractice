021229417fea3e82bb2157ac16c7f28a2daf218b
Patrick Eitel <patrickeitel12@gmail.com>
2023-06-06 16:36:55 -0700

    Reassign all three string variables, add several output statements

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
-var captain = "Jack Sparrow"
-var theChosenOne = "Anakin Skywalker"
-var pumpkinKing = "Jack Skellington"
+var captain = "Barbossa"
+var theChosenOne = "Obi-Wan Kenobi"
+var pumpkinKing = "That Skeleton Guy"
 
 var meaningOfLife = 42
 var lost = 4815162342
@@ -9,3 +9,12 @@ var scouterSays = 9001
 var amIRich = false
 var marioWearsRed = true
 var appleIsExpensive = true
+
+captain = "Jack Sparrow"
+theChosenOne = "Anakin Skywalker"
+pumpkinKing = "Jack Skellington"
+
+console.log(captain, theChosenOne, pumpkinKing)
+console.log(lost)
+console.log(marioWearsRed)
+console.log(scouterSays)
